cb70d774d9162d1bd10424c937d19b11a5778f8e
somayaASS <somaya.assaadi@gmail.com>
2024-01-25 11:40:47 +0100

    create MongoRepository

---
+++ src/main/java/com/produitsconsemetics/ProduitsCosmetics/repository/ProduitRepository.java
@@ -0,0 +1,9 @@
+package com.produitsconsemetics.ProduitsCosmetics.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.produitsconsemetics.ProduitsCosmetics.model.Produit;
+
+public interface ProduitRepository extends MongoRepository<Produit, String> {
+
+}
