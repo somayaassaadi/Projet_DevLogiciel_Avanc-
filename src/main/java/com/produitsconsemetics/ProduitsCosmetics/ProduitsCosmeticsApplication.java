75d3b28933bdf10ad15aba5232271d0421d1c72c
somayaASS <somaya.assaadi@gmail.com>
2024-01-25 11:25:25 +0100

    Create the project Spring Boot

---
+++ src/main/java/com/produitsconsemetics/ProduitsCosmetics/ProduitsCosmeticsApplication.java
@@ -0,0 +1,13 @@
+package com.produitsconsemetics.ProduitsCosmetics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitsCosmeticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsCosmeticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/produitsconsemetics/ProduitsCosmetics/ProduitsCosmeticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produitsconsemetics.ProduitsCosmetics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitsCosmeticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
