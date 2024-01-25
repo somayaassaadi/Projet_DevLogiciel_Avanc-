fab3e72167d4609cbadb3764894502aa7ce4073c
somayaASS <somaya.assaadi@gmail.com>
2024-01-25 11:35:16 +0100

    add attribut to model

--- src/main/java/com/produitsconsemetics/ProduitsCosmetics/model/Produit.java
+++ src/main/java/com/produitsconsemetics/ProduitsCosmetics/model/Produit.java
@@ -1,5 +1,46 @@
 package com.produitsconsemetics.ProduitsCosmetics.model;
 
-public class Produit {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produit {
+	private String id;
+	private String title;
+	private String description;
+	private Long prix;
+	private String iconproduit;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getPrix() {
+		return prix;
+	}
+	public void setPrix(Long prix) {
+		this.prix = prix;
+	}
+	public String getIconproduit() {
+		return iconproduit;
+	}
+	public void setIconproduit(String iconproduit) {
+		this.iconproduit = iconproduit;
+	}
+	
 }
