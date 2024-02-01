c1929eca220904b2710bc23ec829095923d9adf6
somayaASS <somaya.assaadi@gmail.com>
2024-02-01 12:30:12 +0100

    add delete product method

--- src/main/java/com/produitsconsemetics/ProduitsCosmetics/controller/ProduitController.java
+++ src/main/java/com/produitsconsemetics/ProduitsCosmetics/controller/ProduitController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,6 +74,17 @@ public class ProduitController {
 			return new ResponseEntity<>(produitsave, HttpStatus.OK);
 		}else {
 			return new ResponseEntity<>("no product find"+id,HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("produits/{id}")
+	public ResponseEntity<?> deleteproductByid(@PathVariable("id") String id){
+		try {
+			produitRepo.deleteById(id);
+			return new ResponseEntity<>("Successfuly"+id, HttpStatus.OK);
+			
+		}catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
 		}
 	}
 	
